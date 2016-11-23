fa6d7dd293f7724d1b1829866e5fa352be63c084
Asuka Ochi <asukaochi@gmail.com>
2016-11-23 17:02:48 +0000

    Attacking Player 2, done

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,4 +1,3 @@
-require 'spec_helper'
 
 RSpec.feature "Battle" do
 
@@ -11,6 +10,12 @@ RSpec.feature "Battle" do
    scenario "User can see their hit points" do
      sign_in_and_play
      expect(page).to have_content( "Tom: 60HP" )
+   end
+
+   scenario "User gets confirmation of attack" do
+     sign_in_and_play
+     click_button 'ATTACK!'
+     expect(page).to have_content( "John has attacked Tom!" )
    end
 
 end

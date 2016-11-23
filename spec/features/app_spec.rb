48802b09361c75a7f62895616c98e1cb02d9a1c1
Asuka Ochi <asukaochi@gmail.com>
2016-11-23 16:29:31 +0000

    Test Helpers, done

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,18 +1,15 @@
+require 'spec_helper'
+
 RSpec.feature "Battle" do
 
    scenario "User fills out name in form" do
      visit('/')
-     fill_in :player_1, with: "John"
-     fill_in :player_2, with: "Tom"
-     click_button 'Submit'
+     sign_in_and_play
      expect(page).to have_content("John vs. Tom")
    end
 
    scenario "User can see their hit points" do
-     visit('/')
-     fill_in :player_1, with: "John"
-     fill_in :player_2, with: "Tom"
-     click_button 'Submit'
+     sign_in_and_play
      expect(page).to have_content( "Tom: 60HP" )
    end
 

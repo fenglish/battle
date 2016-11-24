395e2a491d84438e64949e236e78ca3fe3917d0d
Asuka Ochi <asukaochi@gmail.com>
2016-11-24 16:45:46 +0000

    Multiplayer, done

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -7,20 +7,35 @@ RSpec.feature "Battle" do
      expect(page).to have_content("John vs. Tom")
    end
 
-   scenario "User can see their hit points" do
+   scenario "Player2(Tom) can see their hit points" do
      sign_in_and_play
      expect(page).to have_content( "Tom: 60HP" )
    end
 
-   scenario "User gets confirmation of attack" do
+    scenario "Player1(John) can see their hit points" do
+      sign_in_and_play
+      expect(page).to have_content( "John: 60HP" )
+    end
+
+   scenario "Player1(John) gets confirmation of attack" do
      sign_in_and_play
      click_button 'ATTACK!'
      expect(page).to have_content( "John has attacked Tom!" )
    end
 
+   scenario "Player2(Tom) gets confirmation of attack" do
+     sign_in_and_play
+     click_button 'ATTACK!'
+     click_button 'ATTACK!'
+     expect(page).to have_content( "Tom has attacked John!" )
+   end
+
   scenario "should show who's turn it is" do
     sign_in_and_play
-    expect(page).to have_content( "It is John's turn" )       
+    expect(page).to have_content( "It is John's turn" )
+  end
+
+  scenario "Users can" do
   end
 
 end
